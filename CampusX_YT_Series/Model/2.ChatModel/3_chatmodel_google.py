07f6d7ca0c856354ea0f56be4852749fb66cb1ea
jhaaakash <aakashkumarjha295@gmail.com>
2025-08-20 09:32:11 +0530

    google model

---
+++ CampusX_YT_Series/Model/2.ChatModel/3_chatmodel_google.py
@@ -0,0 +1,8 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(model="gemini-2.5-pro")
+result = model.invoke("what is the capital of India?")
+print(result.content)
