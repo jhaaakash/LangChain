5ae151e1cc619abeb8012d6b2289571910725898
jhaaakash <aakashkumarjha295@gmail.com>
2025-08-21 08:36:45 +0530

    prompt placeholder

--- CampusX_YT_Series/Prompts/prompt_static_ui.py
+++ CampusX_YT_Series/Prompts/prompt_static_ui.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 load_dotenv()
 
-model = ChatOpenAI(model="gpt-4", temperature=1.5, max_completion_tokens=200)
+model = ChatOpenAI()
 st.header("Welcome to paper Summarizer")
 input = st.text_input("Enter your prompt")
 
